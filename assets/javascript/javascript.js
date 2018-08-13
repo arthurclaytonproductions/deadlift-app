e7752bf277ddd982a74f08e9c2fb8f1586b4810f
arthurclaytonproductions <38487296+arthurclaytonproductions@users.noreply.github.com>
2018-08-13 10:51:57 -0700

    Add files via upload

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -25,67 +25,114 @@ var per = 0;
 var per2 = 0;
 var per3 = 0;
 
-var weeks = ['Week 1', 'Week 2', 'Week 3', 'Week 4', 'Week 5', 'Week 6', 'Week 7', 'Week 8', 'Week 9', 'Week 10', 'Week 11'];
- 
+
   
 const PERCENT_FOR_MAX = 95;
 var percentagesPerWeek = [
   {
     deadlift: 75,
+    deadliftReps: [' - 3x5'],
     deficitDead: 60,
+    deficitDeadReps: [' - 8x3'],
     frontSquat: 60,
+    frontSquatReps: [' - 3x10'],
+    auxillary: [
+      'Stiff Leg Deadlift - 3x8',
+      'Bent Over Row - 3x8',
+      'Lat Pulldown - 3x12'
+    ]
   },
   {
     deadlift: 80,
+    deadliftReps: [' - 3x5'],
     deficitDead: 65,
+    deficitDeadReps: [' - 8x3'],
     frontSquat: 65,
+    frontSquatReps: [' - 3x10'],
+    auxillary: [
+      'Stiff Leg Deadlift - 3x8',
+      'Bent Over Row - 3x8',
+      'Lat Pulldown - 3x12'
+    ]
   },
   {
     deadlift: 85,
+    deadliftReps: [' - 3x5'],
     deficitDead: 70,
+    deficitDeadReps: [' - 8x3'],
     frontSquat: 70,
+    frontSquatReps: [' - 3x10'],
+    auxillary: [
+      'Stiff Leg Deadlift - 3x8',
+      'Bent Over Row - 3x8',
+      'Lat Pulldown - 3x12'
+    ]
   },
   {
     deadlift: 90,
+    deadliftReps: [' - 3x3'],
     deficitDead: 75,
+    deficitDeadReps: [' - 5x3'],
     frontSquat: 75,
+    frontSquatReps: [' - 3x10'],
+    auxillary: [
+      'Stiff Leg Deadlift - 3x8',
+      'Bent Over Row - 3x8',
+      'Lat Pulldown - 3x12'
+    ]
   },
   {
     deadlift: 80,
     deficitDead: 65,
     frontSquat: 65,
+    auxillary: [
+      
+    ]
   },
   {
     deadlift: 85,
     blockPulls: 90,
     frontSquat:80,
+    auxillary: [
+      
+    ]
   },
   {
     deadlift: 90,
     blockPulls: 95,
     frontSquat:80,
+    auxillary: [
+      
+    ]
   },
   {
     deadlift: 95,
     blockPulls: 100,
     frontSquat:85,
+    auxillary: [
+      
+    ]
   },
   {
     deadlift: 100,
     frontSquat:90,
+    auxillary: [
+      
+    ]
   },
   {
     deadlift: 60,
     frontSquat: 95,
+    auxillary: [
+      
+    ]
   },
   {
-    deadlift: 105,
-
+    deadlift: 110,
+    
   },
  
 ];
-
-
 
 //functions
 //only allow numbers in the input form
@@ -113,37 +160,33 @@ $(document).bind('keypress', function(e) {
           
     }
 });
-
-
-//squat and deadlift training percentages
-$('#addmaxes').on('click',function(){
-    $('#program').empty();
-    dlMax = $('#dlcurrentmax-input').val().trim();
-    ftsqMax = $('#ftsqcurrentmax-input').val().trim();
-    per = 95;
-    dlTrainingMax = (dlMax * per) / 100;
-    frsqTrainingMax = (ftsqMax * per) / 100;
-    var roundedNumberDl = Math.floor(dlTrainingMax);
-    var roundedNumberSq = Math.floor(frsqTrainingMax);
-    var newDiv = $('<div>');
-    newDiv.addClass('workingmaxes');
-    newDiv.html('<h2>10 Week Training Routine ' + '<br/><h3>Deadlift Training Max(95% of max): ' + roundedNumberDl + ' <br/>Front Squat Training Max(95% of max): ' + roundedNumberSq) ;
-    $('#program').css({'color': 'rgb(255, 255, 255)',
-        'padding': '2rem 1rem' ,
-        'margin-bottom': '2rem',
-        'border-radius': '0.3rem',
-        'background': '#000064', 
-        'background': '-webkit-linear-gradient(to right, #1BADD8, #00003E)', 
-        'background': 'linear-gradient(to right, #1BADD8, #00003E)'});
-    
-    $('#program').append(newDiv); 
-
-
-
-        
-});   
-$('#addmaxes').on('click',function(){
-  for(var i = 0, j = 0, k = 0; i < percentagesPerWeek.length && j < weeks.length; i++, j++){
+// training max and css
+function setMax() {
+  dlMax = $('#dlcurrentmax-input').val().trim();
+  ftsqMax = $('#ftsqcurrentmax-input').val().trim();
+  per = 95;
+  dlTrainingMax = (dlMax * per) / 100;
+  frsqTrainingMax = (ftsqMax * per) / 100;
+  var roundedNumberDl = Math.floor(dlTrainingMax);
+  var roundedNumberSq = Math.floor(frsqTrainingMax);
+  var newDiv = $('<div>');
+  newDiv.addClass('workingmaxes');
+  newDiv.html('<h2>10 Week Training Routine ' +
+   '<br/><h3>Deadlift Training Max(95% of max): ' +
+   roundedNumberDl + ' <br/>Front Squat Training Max(95% of max): ' + roundedNumberSq) ;
+  $('#program').css({'color': 'rgb(255, 255, 255)',
+      'padding': '2rem 1rem' ,
+      'margin-bottom': '2rem',
+      'border-radius': '0.3rem',
+      'background': '#000064', 
+      'background': '-webkit-linear-gradient(to right, #1BADD8, #00003E)', 
+      'background': 'linear-gradient(to right, #1BADD8, #00003E)'});
+  
+  $('#program').append(newDiv); 
+}
+// weekly workouts
+function createWorkout() {
+  for(var i = 0, j = 0; i < percentagesPerWeek.length && j < 11; i++, j++){
     dlMax = $('#dlcurrentmax-input').val().trim();
     ftsqMax = $('#ftsqcurrentmax-input').val().trim();
     dlTrainingMax = (dlMax * PERCENT_FOR_MAX) / 100;
@@ -151,44 +194,49 @@ $('#addmaxes').on('click',function(){
     var deadliftPercent = percentagesPerWeek[i].deadlift;
     var weekDl = (dlTrainingMax * deadliftPercent) / 100;
     var rnWeekDl = Math.floor(weekDl);
+    var dlReps = percentagesPerWeek[i].deadliftReps;
     var defictDeadPercent = percentagesPerWeek[i].deficitDead;
     var weekDef = (dlTrainingMax * defictDeadPercent) / 100;
     var rnDefWeek = Math.floor(weekDef);
+    var dlDefReps = percentagesPerWeek[i].deficitDeadReps;
     var blockPullPercent = percentagesPerWeek[i].blockPulls;
     var weekBlockPulls = (dlTrainingMax * blockPullPercent) / 100;
     var rnBlockPullsWeek = Math.floor(weekBlockPulls);
     var frontSquatPercent = percentagesPerWeek[i].frontSquat;
     var weekSq = (frsqTrainingMax * frontSquatPercent) / 100;
     var rnWeekSq = Math.floor(weekSq);
+    var fnsqReps = percentagesPerWeek[i].frontSquatReps;
     var newDivOne = $('<div>');
     var newDivTwo = $('<div>');
+    var auxData = '';
+    
+    
+    if(percentagesPerWeek[i].auxillary) {
+     
+      auxData =  percentagesPerWeek[i].auxillary.reduce((htmlStr,auxlift) => {
+          return htmlStr + '<br/>' + auxlift;
+      }, '');
+    }
+    
     newDivOne.addClass('trainingProgram');
     newDivTwo.addClass('trainingProgram');
-    newDivOne.html('<h3>' + weeks[j]);
+    newDivOne.html('<h3>Week ' + (j + 1));
     $('#program').append(newDivOne);
-    
+    $('#program').append(newDivTwo);
+    newDivTwo.html(
+      'Deadlift('+deadliftPercent+'%): at ' + rnWeekDl +   dlReps +
+      '<br/>Deficit Deadlift('+defictDeadPercent+'%): at ' + rnDefWeek + dlDefReps +
+      '<br/>Front Squat('+frontSquatPercent+'%): at ' + rnWeekSq + fnsqReps +
+      auxData
+    );
   }
-  
+}
+// starts functions calls them
+$('#addmaxes').on('click',function(){
+  $('#program').empty();
+  setMax();
+  createWorkout();
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //Contact form to firebase
 $("#contact-form").submit(function(config) { $(this), console.log("Submit to Firebase");
